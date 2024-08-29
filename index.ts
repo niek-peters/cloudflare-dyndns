984910dbcd6f056e33fc794adcda97b14c30d343
Niek Peters <n.s.peters@student.utwente.nl>
2024-08-29 16:47:55 +0200

    Wrote a script that updates A records on Cloudflare (dyndns script)

---
+++ index.ts
@@ -0,0 +1,103 @@
+const DOMAINS = process.env.DOMAINS!.split(",");
+const TOKEN = process.env.CLOUDFLARE_API_TOKEN;
+const MINUTE_INTERVAL = parseInt(process.env.MINUTE_INTERVAL!);
+
+run();
+setInterval(run, 1000 * 60 * MINUTE_INTERVAL);
+
+async function run() {
+  try {
+    const ip = await getIP();
+
+    const zones = await getZones();
+    for (const zone of zones) {
+      let records;
+      try {
+        records = await getRecords(zone.name, zone.id);
+      } catch (e) {
+        console.error(`ERROR: ${(e as Error).message}`);
+      }
+      for (const record of records) {
+        if (DOMAINS.includes(record.name) && record.type === "A") {
+          if (record.content === ip)
+            console.log(
+              `SUCCESS: A record for '${record.name}' was already set to '${ip}'`
+            );
+          else
+            try {
+              await updateRecord(record.name, zone.id, record.id, ip);
+            } catch (e) {
+              console.error(`ERROR: ${(e as Error).message}`);
+            }
+        }
+      }
+    }
+  } catch (e) {
+    console.error(`ERROR: ${(e as Error).message}`);
+  }
+}
+
+async function getIP() {
+  const res = await fetch("https://api.ipify.org/");
+
+  if (!res.ok) throw new Error(`Could not get IP | ${await res.text()}`);
+
+  return await res.text();
+}
+
+async function getZones() {
+  const res = await fetch("https://api.cloudflare.com/client/v4/zones", {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+
+  if (!res.ok) throw new Error(`Could not get zones | ${await res.text()}`);
+
+  return (await res.json()).result;
+}
+
+async function getRecords(zoneName: string, zoneId: string) {
+  const res = await fetch(
+    `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    }
+  );
+
+  if (!res.ok)
+    throw new Error(
+      `Could not get records for zone '${zoneId}' (${zoneName}) | ${await res.text()}`
+    );
+
+  return (await res.json()).result;
+}
+
+async function updateRecord(
+  domain: string,
+  zoneId: string,
+  recordId: string,
+  ip: string
+) {
+  const res = await fetch(
+    `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records/${recordId}`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      body: JSON.stringify({
+        content: ip,
+      }),
+    }
+  );
+
+  if (!res.ok)
+    throw new Error(
+      `Could not update A record for '${domain}' to '${ip}' | ${await res.text()}`
+    );
+
+  console.log(`SUCCESS: Updated A record for '${domain}' to '${ip}'`);
+}
